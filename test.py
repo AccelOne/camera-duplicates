0318177747214bc56e0caf4217734c929f27b6b9
marianomontero <etmaro@gmail.com>
2019-04-05 22:59:48 -0300

    Update test.py

--- test.py
+++ test.py
@@ -3,4 +3,6 @@ from duplicates import DataSanitization
 
 data = DataSanitization("duplicates.txt")
 print(data.sanitize())
-
+print(data.manual_delete("iPhone"))
+print(data.manual_replacement("5dMark III", "5D Mark III"))
+print(data.renamed_by_sanitized)
